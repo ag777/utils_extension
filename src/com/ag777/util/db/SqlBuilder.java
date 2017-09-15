782e2d404e051ebc15e876df74f2c75ce672e37d
wanggz <ag777@vip.qq.com>
2017-09-15 17:57:51 +0800

    代码更新

--- src/com/ag777/util/db/SqlBuilder.java
+++ src/com/ag777/util/db/SqlBuilder.java
@@ -10,9 +10,10 @@ import com.ag777.util.lang.model.Pair;
 public class SqlBuilder {
 	private String tableName;
 	private List<Pair<ColumnPojo, String>> columnPairList;	//first为数据库中的字段信息，second为javabean中的变量名
-	private String insert;
-	private String update;
-	private String create;
+	private Sql insert;
+	private Sql update;
+	private Sql create;
+	
 
 	public SqlBuilder(String tableName, List<Pair<ColumnPojo, String>> columnPairList) {
 		super();
@@ -20,33 +21,33 @@ public class SqlBuilder {
 		this.columnPairList = columnPairList;
 	}
 
-	public String getInsert() {
+	public Sql getInsert() {
 		if(insert == null) {
 			synchronized (SqlBuilder.class) {
 				if(insert == null) {
-					insert = getInsertSql();
+					initInsertSql();
 				}
 			}
 		}
 		return insert;
 	}
 	
-	public String getUpdate() {
+	public Sql getUpdate() {
 		if(update == null) {
 			synchronized (SqlBuilder.class) {
 				if(update == null) {
-					update = getUpdateSql();
+					initUpdateSql();
 				}
 			}
 		}
 		return update;
 	}
 	
-	public String getCreate() {
+	public Sql getCreate() {
 		if(create == null) {
 			synchronized (SqlBuilder.class) {
 				if(create == null) {
-					create = getCreateTableSql();
+					initCreateTableSql();
 				}
 				
 			}
@@ -55,17 +56,18 @@ public class SqlBuilder {
 	}
 	
 	/**
-	 * 获取插入语句
+	 * 构造插入语句
 	 * @param tableName
 	 * @param columnNameList
-	 * @return
 	 */
-	private String getInsertSql() {
+	private void initInsertSql() {
+		List<String> insertParamList = new ArrayList<>();
 		StringBuilder sb_insert = new StringBuilder();
 		sb_insert.append("INSERT INTO ").append(tableName).append(" (");
 		for (Pair<ColumnPojo, String> item : columnPairList) {
 			String colName = item.first.getName();
 			sb_insert.append(' ').append(colName).append(',');
+			insertParamList.add(item.second);
 		}
 		sb_insert.setLength(sb_insert.length()-1);
 		sb_insert.append(" ) VALUES (");
@@ -74,14 +76,47 @@ public class SqlBuilder {
 		}
 		sb_insert.setLength(sb_insert.length()-1);
 		sb_insert.append(" );");
-		return sb_insert.toString();
+		insert =  new Sql(sb_insert.toString(), insertParamList);
+	}
+	
+	/**
+	 * 构造update语句
+	 */
+	private void initUpdateSql() {
+		List<String> updateParamList = new ArrayList<>();
+		
+		StringBuilder sb = new StringBuilder();
+		List<Pair<ColumnPojo, String>> pkList = new ArrayList<>();	//主键列表
+		sb.append("UPDATE ").append(tableName).append(" SET");
+		
+		for (Pair<ColumnPojo, String> pair : columnPairList) {
+			ColumnPojo columnPojo = pair.first;
+			if(columnPojo.isPK()) {
+				pkList.add(pair);
+			} else {
+				sb.append(" ").append(columnPojo.getName()).append("=?,");
+				updateParamList.add(pair.second);
+			}
+		}
+		if(",".equals(sb.charAt(sb.length()-1))) {
+			sb.setLength(sb.length()-1);
+		}
+		sb.append(" WHERE");
+		if(!pkList.isEmpty()) {
+			for (Pair<ColumnPojo, String> pk : pkList) {
+				sb.append(" ").append(pk.first.getName()).append("=? and");
+				updateParamList.add(pk.second);
+			}
+			sb.setLength(sb.length()-4);
+		}
+		
+		update = new Sql(sb.toString(), updateParamList);
 	}
 	
 	/**
 	 * 构造建表语句
-	 * @return
 	 */
-	private String getCreateTableSql() {
+	private void initCreateTableSql() {
 		StringBuilder sb = new StringBuilder();
 		List<String> pkList = new ArrayList<>();	//主键列表
 		sb.append("CREATE TABLE ")
@@ -127,37 +162,16 @@ public class SqlBuilder {
 			sb.setLength(sb.length()-1);
 		}
 		sb.append(");");
-		return sb.toString();
+		create =  new Sql(sb.toString(), null);
 	}
 	
-	/**
-	 * 获取update语句
-	 * @return
-	 */
-	private String getUpdateSql() {
-		StringBuilder sb = new StringBuilder();
-		List<String> pkList = new ArrayList<>();	//主键列表
-		sb.append("UPDATE ").append(tableName).append(" SET");
-		
-		for (Pair<ColumnPojo, String> pair : columnPairList) {
-			ColumnPojo columnPojo = pair.first;
-			if(columnPojo.isPK()) {
-				pkList.add(columnPojo.getName());
-			} else {
-				sb.append(" ").append(columnPojo.getName()).append("=?,");
-			}
+	public class Sql {
+		public String sql;
+		public List<String> paramList;
+		public Sql(String sql, List<String> paramList) {
+			super();
+			this.sql = sql;
+			this.paramList = paramList;
 		}
-		if(",".equals(sb.charAt(sb.length()-1))) {
-			sb.setLength(sb.length()-1);
-		}
-		sb.append(" WHERE");
-		if(!pkList.isEmpty()) {
-			for (String pkName : pkList) {
-				sb.append(" ").append(pkName).append("=? and");
-			}
-			sb.setLength(sb.length()-4);
-		}
-		
-		return sb.toString();
 	}
 }
