635e81aab476baf5b498b8a9755fa21a411ce081
wanggz <ag777@vip.qq.com>
2017-12-01 16:13:05 +0800

    代码更新

--- src/com/ag777/util/db/DbUtils.java
+++ src/com/ag777/util/db/DbUtils.java
@@ -24,20 +24,22 @@ public class DbUtils {
 	 * @throws Exception
 	 */
 	public static boolean copyDb(DbHelper source, DbHelper target) throws Exception {
+		/**
+		 * 思路:
+		 * 1.取出数据库中所有的表(排除系统表)
+		 * 2.在目标数据库中创建这些表(原来存在的表直接删掉)
+		 * 3.取出原表数据,插入新表
+		 */
 		ArrayList<String> tableList = source.tableList();
+		tableList.remove("meta");	//排除系统表
+		
 		for (String tableName : tableList) {	//创建表
-			if("meta".equals(tableName)) {
-				continue;
-			}
 			List<ColumnPojo> colList = source.columnList(tableName);
 			SqlBuilder sb = new SqlBuilder(tableName, colList, false);
 			target.update("DROP TABLE IF EXISTS "+tableName);
 			sb.doCreate(target);
 		}
 		for (String tableName : tableList) {	//插入数据
-			if("meta".equals(tableName)) {
-				continue;
-			}
 			List<ColumnPojo> colList = source.columnList(tableName);
 			SqlBuilder sb = new SqlBuilder(tableName, colList, false);
 			if(!copyData(tableName, sb, source, target)) {
@@ -48,12 +50,21 @@ public class DbUtils {
 		return true;
 	}
 	
-	private static boolean copyData(String tableName, SqlBuilder sb, DbHelper helper, DbHelper helper2) throws Exception {
+	/**
+	 * 复制表内数据
+	 * @param tableName
+	 * @param sb
+	 * @param helper
+	 * @param helper2
+	 * @return
+	 * @throws Exception
+	 */
+	private static boolean copyData(String tableName, SqlBuilder sb, DbHelper source, DbHelper target) throws Exception {
 		System.out.println("开始复制表:"+tableName);
 		
-		List<Map<String, Object>> list = helper.queryList("select * from "+tableName);
+		List<Map<String, Object>> list = source.queryList("select * from "+tableName);
 				
-		return helper2.doTransaction(h->{
+		return target.doTransaction(h->{
 			for (Map<String, Object> map : list) {
 				if(sb.doInsert(map, h) < 0) {
 					return false;
