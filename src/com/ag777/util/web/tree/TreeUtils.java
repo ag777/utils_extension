ff26c4140e33363eb8d7b6273ece5e297947a292
wanggz <ag777@vip.qq.com>
2018-08-29 18:26:14 +0800

    完善工具类

--- src/com/ag777/util/web/tree/TreeUtils.java
+++ src/com/ag777/util/web/tree/TreeUtils.java
@@ -2,7 +2,6 @@ package com.ag777.util.web.tree;
 
 import java.util.List;
 import java.util.Map;
-
 import com.ag777.util.lang.collection.ListUtils;
 import com.ag777.util.lang.collection.MapUtils;
 
@@ -10,9 +9,58 @@ import com.ag777.util.lang.collection.MapUtils;
  * 树结构工具类
  * 
  * @author ag777
- * @version create on 2018年08月09日,last modify at 2018年08月10日
+ * @version create on 2018年08月09日,last modify at 2018年08月29日
  */
 public class TreeUtils {
+	
+	/**
+	 * 遍历树结构
+	 * @param node
+	 * @param viewer
+	 */
+	public static <S>void treeForeach(S node, ViewerT<S> viewer) {
+		treeForeach(node, viewer, 0);
+	}
+	
+	private static <S>void treeForeach(S node, ViewerT<S> viewer, int deep) {
+		List<S> children = viewer.children(node, deep);
+		if(ListUtils.isEmpty(children)) {	//有子节点
+			viewer.view(node, false, deep);
+			for (S child : children) {
+				treeForeach(child, viewer, deep+1);
+			}
+		} else {	//叶节点
+			viewer.view(node, true, deep);
+		}
+		
+	}
+	
+	/**
+	 * 遍历列表结构
+	 * @param itemList
+	 * @param rootId
+	 * @param viewer
+	 */
+	public static<S>void listForeach(List<S> itemList, Object rootId, ViewerL<S> viewer) {
+		/*先将列表转化为{pid:List<列表项>}map*/
+		Map<Object, List<S>> pidItemMap = getPidItemMap(itemList, viewer);
+		List<S> children= MapUtils.get(pidItemMap, rootId);
+		for (S child : children) {
+			listForeach(child, pidItemMap, 0, viewer);
+		}
+	}
+	
+	private static<S>void listForeach(S node, Map<Object, List<S>> pidItemMap, int deep, ViewerL<S> viewer) {
+		Object id = viewer.getId(node);
+		boolean isLeaf = !pidItemMap.containsKey(id);
+		viewer.view(node, isLeaf, deep);
+		if(isLeaf) {
+			List<S> children = MapUtils.get(pidItemMap, id);
+			for (S child : children) {
+				listForeach(child, pidItemMap, deep+1, viewer);
+			}
+		}
+	}
 	
 	/**
 	 * 
@@ -20,7 +68,7 @@ public class TreeUtils {
 	 * @param convertor
 	 * @return
 	 */
-	public static <T, S>List<T> tree2List(List<S> treeList, Convertor<T,S> convertor) {
+	public static <T, S>List<T> tree2List(List<S> treeList, ConvertorT2L<T,S> convertor) {
 		return new TreeUtils().tree2List2(treeList, convertor);
 	}
 	
@@ -31,16 +79,9 @@ public class TreeUtils {
 	 * @param convertor 转换工具类
 	 * @return
 	 */
-	public static <T, S>List<T> list2Tree(List<S> itemList, Object rootId, Convertor1<T,S> convertor) {
+	public static <T, S>List<T> list2Tree(List<S> itemList, Object rootId, ConvertorL2T<T,S> convertor) {
 		/*先将列表转化为{pid:List<列表项>}map, 这里不能直接转化为树节点，因为会丢失id和pid信息*/
-		Map<Object, List<S>> pidItemMap = MapUtils.newHashMap();
-		for (S item : itemList) {
-			Object pid = convertor.getPid(item);
-			if(!pidItemMap.containsKey(pid)) {
-				pidItemMap.put(pid, ListUtils.newArrayList());
-			}
-			pidItemMap.get(pid).add(item);	
-		}
+		Map<Object, List<S>> pidItemMap = getPidItemMap(itemList, convertor);
 		
 		List<T> treeList = ListUtils.newArrayList();
 		List<S> itemList1 = MapUtils.get(pidItemMap, rootId);
@@ -48,8 +89,12 @@ public class TreeUtils {
 			return treeList;
 		}
 		for (S item : itemList1) {
-			T tree = convertor.toNode(item);
-			fillTreeNode(tree, convertor.getId(item), pidItemMap, convertor);
+			List<S> children = MapUtils.get(pidItemMap, convertor.getId(item));
+			boolean isLeaf = ListUtils.isEmpty(children);	//叶节点，没有子节点
+			T tree = convertor.toNode(item, isLeaf);
+			if(!isLeaf) {
+				fillTreeNode(tree, children, pidItemMap, convertor);
+			}
 			treeList.add(tree);
 		}
 		return treeList;
@@ -58,20 +103,21 @@ public class TreeUtils {
 	/**
 	 * [递归]找出该节点下的子节点，转换并添加至该节点下
 	 * @param treeNode 当前树节点
-	 * @param id 当前树节点所对应id
+	 * @param itemChildren 当前项所有子项
 	 * @param pidItemMap 父节点及其对应的列表(源)
 	 * @param convertor 转换工具
 	 */
-	private static <T, S>void fillTreeNode(T treeNode, Object id, Map<Object, List<S>> pidItemMap, Convertor1<T,S> convertor) {
-		List<S> itemChildren = MapUtils.get(pidItemMap, id);
-		if(ListUtils.isEmpty(itemChildren)) {	//没有子节点了
-			return;
-		}
+	private static <T, S>void fillTreeNode(T treeNode, List<S> itemChildren, Map<Object, List<S>> pidItemMap, ConvertorL2T<T,S> convertor) {
 		List<T> children = ListUtils.newArrayList();	//子节点
 		for (S item : itemChildren) {
-			T node = convertor.toNode(item);
+			List<S> childList = MapUtils.get(pidItemMap, convertor.getId(item));
+			boolean isLeaf = ListUtils.isEmpty(childList);	//叶节点，没有子节点
+			T node = convertor.toNode(item, isLeaf);
 			children.add(node);
-			fillTreeNode(node, convertor.getId(item), pidItemMap, convertor);
+			if(!isLeaf) {
+				fillTreeNode(node, childList, pidItemMap, convertor);
+			}
+			
 		}
 		convertor.setChildren(children, treeNode);	//将所有子节点添加至该节点下
 	}
@@ -89,7 +135,7 @@ public class TreeUtils {
 	 * @param treeMap
 	 * @return
 	 */
-	public <T, S>List<T> tree2List2(List<S> treeList, Convertor<T,S> convertor) {
+	public <T, S>List<T> tree2List2(List<S> treeList, ConvertorT2L<T,S> convertor) {
 		List<T> list = ListUtils.newArrayList();
 		id = 0;
 		long pid=0;
@@ -105,7 +151,7 @@ public class TreeUtils {
 	 * @param convertor 转化辅助工具
 	 * @return
 	 */
-	public <T, S>void addList(List<S> treeList, List<T> itemList, long pid, Convertor<T,S> convertor) {
+	public <T, S>void addList(List<S> treeList, List<T> itemList, long pid, ConvertorT2L<T,S> convertor) {
 		for (S item : treeList) {
 			List<S> children = convertor.getChildren(item);
 			boolean isLeaf = ListUtils.isEmpty(children);	//没有子节点被认为是叶节点
@@ -123,13 +169,46 @@ public class TreeUtils {
 	}
 	
 	/**
-	 * 转换中介类
+	 * 列表型树建立pid:节点列表的映射关系
+	 * @param itemList
+	 * @param getter
+	 * @return
+	 */
+	public static <S>Map<Object, List<S>> getPidItemMap(List<S> itemList, PidGetter<S> getter) {
+		Map<Object, List<S>> pidItemMap = MapUtils.newHashMap();
+		for (S item : itemList) {
+			Object pid = getter.getPid(item);
+			if(!pidItemMap.containsKey(pid)) {
+				pidItemMap.put(pid, ListUtils.newArrayList());
+			}
+			pidItemMap.get(pid).add(item);	
+		}
+		return pidItemMap;
+	}
+	
+	/**
+	 * 获取pid接口
+	 * @author wanggz
+	 *
+	 * @param <S>
+	 */
+	public static interface PidGetter<S> {
+		/**
+		 * 从(源)列表项中取出关联父节点的键,注意不能为null
+		 * @param item
+		 * @return
+		 */
+		public Object getPid(S item);
+	}
+	
+	/**
+	 * 转换中介接口(Tree->List)
 	 * @author wanggz
 	 *
 	 * @param <T> 目标(也就是最终要返回的结果项)
 	 * @param <S> 源
 	 */
-	public static interface Convertor<T, S>  {
+	public static interface ConvertorT2L<T, S>  {
 		/**
 		 * 转换为目标列表中的项
 		 * @param id
@@ -144,7 +223,14 @@ public class TreeUtils {
 	}
 	
 	
-	public static interface Convertor1<T, S>  {
+	/**
+	 * 转换中介接口(List->Tree)
+	 * @author wanggz
+	 *
+	 * @param <T>
+	 * @param <S>
+	 */
+	public static interface ConvertorL2T<T, S> extends PidGetter<S>{
 		/**
 		 * 从(源)列表项中取出主键,注意不能为null
 		 * @param item
@@ -152,19 +238,15 @@ public class TreeUtils {
 		 */
 		public Object getId(S item);
 		
-		/**
-		 * 从(源)列表项中取出关联父节点的键,注意不能为null
-		 * @param item
-		 * @return
-		 */
-		public Object getPid(S item);
+//		public Object getPid(S item);
 		
 		/**
 		 * 转化为目标树中的节点
 		 * @param item
+		 * @param isLeaf 是否为叶节点(没有子节点)
 		 * @return
 		 */
-		public T toNode(S item);
+		public T toNode(S item, boolean isLeaf);
 		
 		/**
 		 * 为当前节点增加子节点
@@ -173,5 +255,28 @@ public class TreeUtils {
 		 * @return
 		 */
 		public T setChildren(List<T> children, T node);
+	}
+
+	/**
+	 * 树结构遍历接口
+	 * @author wanggz
+	 *
+	 * @param <S>
+	 */
+	public static interface ViewerT<S> {
+		public List<S> children(S node, int deep);
+		public void view(S node, boolean isLeaf, int deep);
+	}
+	
+	/**
+	 * 列表结构遍历接口
+	 * @author wanggz
+	 *
+	 * @param <S>
+	 */
+	public static interface ViewerL<S> extends PidGetter<S> {
+		public Object getId(S item);
+//		public Object pid(S item);
+		public Object view(S node, boolean isLeaf, int deep);
 	}
 }
--- src/com/ag777/util/web/tree/ZtreeUtils.java
+++ src/com/ag777/util/web/tree/ZtreeUtils.java
@@ -1,6 +1,7 @@
 package com.ag777.util.web.tree;
 
 import java.util.List;
+import java.util.Map;
 
 import com.ag777.util.lang.Console;
 import com.ag777.util.lang.collection.ListUtils;
@@ -12,20 +13,20 @@ import com.ag777.util.web.tree.model.ZTreeItem;
  * 树结构(ztree)工具类
  * 
  * @author ag777
- * @version create on 2018年08月09日,last modify at 2018年08月10日
+ * @version create on 2018年08月09日,last modify at 2018年08月29日
  */
 public class ZtreeUtils {
 
 	private ZtreeUtils() {}
 	
 	/**
-	 * 树转项列表
+	 * 树转项列表(ztree->ztree)
 	 * @param treeList
 	 * @return
 	 */
 	public static List<ZTreeItem> tree2List(List<ZTree> treeList) {
 		
-		return TreeUtils.tree2List(treeList, new TreeUtils.Convertor<ZTreeItem, ZTree>() {
+		return TreeUtils.tree2List(treeList, new TreeUtils.ConvertorT2L<ZTreeItem, ZTree>() {
 
 			@Override
 			public ZTreeItem getItem(long id, long pid, ZTree node, boolean isLeaf) {
@@ -47,43 +48,93 @@ public class ZtreeUtils {
 		
 	}
 	
-	/**
-	 * 项转树列表
-	 * @param itemList
-	 * @param rootId
-	 * @return
-	 */
-	public static List<ZTree> list2Tree(List<ZTreeItem> itemList, long rootId) {
+	public static <S>List<ZTree> list2Tree(List<S> itemList, long rootId, Converter<S> converter) {
 		return TreeUtils.list2Tree(itemList, rootId, 
-				new TreeUtils.Convertor1<ZTree, ZTreeItem>() {
+				new TreeUtils.ConvertorL2T<ZTree, S>() {
 
 					@Override
-					public Object getId(ZTreeItem item) {
-						return item.getId();
+					public Object getId(S item) {
+						return converter.id(item);
 					}
 
 					@Override
-					public Object getPid(ZTreeItem item) {
-						return item.getpId();
+					public Object getPid(S item) {
+						return converter.pid(item);
 					}
 
 					@Override
-					public ZTree toNode(ZTreeItem item) {
+					public ZTree toNode(S item, boolean isLeaf) {
 						return new ZTree()
-								.setName(item.getName())
-								.setName(item.getName())
-								.setOpen(item.getOpen())
-								.setIcon(item.getIcon())
-								.setIsParent(item.getIsParent())
-								.setExtraData(item.getExtraData());
+								.setName(converter.name(item))
+								.setOpen(converter.open(item, isLeaf))
+								.setIcon(converter.icon(item, isLeaf))
+								.setIsParent(converter.isParent(item, isLeaf))
+								.setExtraData(converter.extraData(item, isLeaf));
 					}
 
 					@Override
 					public ZTree setChildren(List<ZTree> children, ZTree node) {
 						return node.setChildren(children);
 					}
+				
 				}
-			);
+		);
+	}
+	
+	/**
+	 * 项转树列表(ztree->ztree)
+	 * @param itemList
+	 * @param rootId
+	 * @return
+	 */
+	public static List<ZTree> list2Tree(List<ZTreeItem> itemList, long rootId) {
+		return list2Tree(itemList, rootId, new Converter<ZTreeItem>() {
+
+			@Override
+			public String name(ZTreeItem item) {
+				return item.getName();
+			}
+
+			@Override
+			public long id(ZTreeItem item) {
+				return item.getId();
+			}
+
+			@Override
+			public long pid(ZTreeItem item) {
+				return item.getpId();
+			}
+
+			@Override
+			public Boolean open(ZTreeItem item, boolean isLeaf) {
+				return item.getOpen();
+			}
+
+			@Override
+			public Boolean isParent(ZTreeItem item, boolean isLeaf) {
+				return item.getIsParent();
+			}
+
+			@Override
+			public String icon(ZTreeItem item, boolean isLeaf) {
+				return item.getIcon();
+			}
+
+			@Override
+			public Map<String, Object> extraData(ZTreeItem item, boolean isLeaf) {
+				return item.getExtraData();
+			}
+		});
+	}
+	
+	public static interface Converter<S> {
+		public String name(S item);
+		public long id(S item);
+		public long pid(S item);
+		public Boolean open(S item, boolean isLeaf);
+		public Boolean isParent(S item, boolean isLeaf);
+		public String icon(S item, boolean isLeaf);
+		public Map<String, Object> extraData(S item, boolean isLeaf);
 	}
 	
 	public static void main(String[] args) {
