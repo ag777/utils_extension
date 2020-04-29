6821270e6c0bd4add2f7b223da920520f184bc6b
ag777 <aaaggg777@qq.com>
2020-04-29 15:19:19 +0800

    ftp工具类大改

--- src/com/ag777/util/remote/ftp/FtpHelper.java
+++ src/com/ag777/util/remote/ftp/FtpHelper.java
@@ -1,11 +1,14 @@
 package com.ag777.util.remote.ftp;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketException;
 import java.nio.charset.Charset;
+import java.util.List;
+
 import javax.naming.AuthenticationException;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -16,6 +19,7 @@ import com.ag777.util.lang.IOUtils;
 import com.ag777.util.lang.StringUtils;
 import com.ag777.util.lang.collection.ListUtils;
 import com.ag777.util.lang.exception.Assert;
+import com.ag777.util.lang.exception.model.ValidateException;
 import com.ag777.util.lang.interf.Disposable;
 import com.ag777.util.lang.model.Charsets;
 
@@ -30,10 +34,11 @@ import com.ag777.util.lang.model.Charsets;
  * </ul>
  * </p>
  * 关于主动模式和被动模式的参考资料:https://www.cnblogs.com/xiaohh/p/4789813.html
+ * 中文乱码问题解决参考:https://blog.csdn.net/qq_35170213/article/details/80284730
  * </p>
  * 
  * @author ag777
- * @version create on 2018年04月13日,last modify at 2020年04月26日
+ * @version create on 2018年04月13日,last modify at 2020年04月29日
  */
 public class FtpHelper implements Disposable {
 
@@ -47,6 +52,11 @@ public class FtpHelper implements Disposable {
 	public FtpHelper(FTPClient client) {
 		this.client = client;
 		modeLocalPassiveMode(localPassiveMode);
+		client.setControlEncoding(ENCODINGS[0].toString());
+		try {
+			client.setFileType(FTPClient.BINARY_FILE_TYPE);
+		} catch (IOException e) {
+		}
 	}
 	
 	@Deprecated
@@ -163,85 +173,132 @@ public class FtpHelper implements Disposable {
 	}
 	
 	/**
-	 * 上传文件或目录
-	 * 
-	 * @param localFilePath 本地文件路径
-	 * @param targetDirs 逐级目录,比如a/b/就是{"a","b"},根目录传null
+	 * 上传文件
+	 * @param localFiles 本地文件列表
+	 * @param targetDir 指定的远程目录
 	 * @return
+	 * @throws FileNotFoundException 本地文件不存在
 	 * @throws IOException
 	 */
-	public synchronized boolean uploadFile(String localFilePath, String[] targetDirs) throws IOException {
-		Assert.notNull(client, "该ftpClient已经被释放");
-		Assert.notExisted(localFilePath, "需要ftp传输的文件不存在:"+localFilePath);
-		
-		try {
-			if(!ListUtils.isEmpty(targetDirs)) {	//逐级进入对应目录并创建文件夹
-				for (String dir : targetDirs) {
-					dir = encode(dir+File.separator);
-					client.makeDirectory(dir);
-					client.changeWorkingDirectory(dir);
-				}
-				
+	public boolean uploadFile(File[] localFiles, String targetDir) throws FileNotFoundException, IOException {
+		/*文件判存*/
+		for (File file : localFiles) {
+			if(!file.exists()) {
+				throw new FileNotFoundException("file not existed:"+file.getAbsolutePath());
 			}
-			return uploadFile(localFilePath, (String)null);
+		}
+		/*文件判存 end*/
+		String currentWorkingDir = client.printWorkingDirectory();
+		mkDirs(targetDir, false); //创建文件夹并进入对应目录
+		try {
+			for (File file : localFiles) {
+				if(file.isDirectory()) {		//文件夹
+					String dir = file.getName()+File.separator;
+					File[] subFiles = file.listFiles();
+					if(ListUtils.isEmpty(subFiles)) {	//空文件夹
+						mkDirs(dir, true); //创建文件夹并返回当前目录
+					} else {	//含有子文件,则递归上传
+						boolean flag = uploadFile(subFiles, dir);
+						if(!flag) {
+							return false;
+						}
+					}
+					
+				} else {
+					boolean flag = uploadSingleFile(file.getAbsolutePath(), file.getName());
+					if(!flag) {
+						Console.err("ftp传输失败:"+file.getAbsolutePath());
+						return false;
+					}
+				}
+			}
+			return true;
 		} catch(Exception ex) {
 			throw ex;
 		} finally {
-			if(targetDirs != null) {	//为了在操作完成后回到根目录
-				for (int i=0; i<targetDirs.length; i++) {
-					client.changeToParentDirectory();
-				}
-				
-			}
+			changeWorkingDirectory(currentWorkingDir);
 		}
 	}
 	
 	/** 
      * 下载文件
-     * @param remoteFileName 
-     * @param locaFileName 
+     * @param remoteFileName 远程文件路径
+     * @param locaFileName 保存到本地文件路径
+     * @throws FileNotFoundException 文件不存在
 	 * @throws IOException 
      */  
     public void download(String targetFilePath,  
-            String locaFilePath) throws IOException {  
+            String locaFilePath) throws FileNotFoundException, IOException {  
     	Assert.notNull(client, "该ftpClient已经被释放");
-    	
+    	if(!fileExist(targetFilePath)) {
+    		throw new FileNotFoundException("file not existed:"+targetFilePath);
+    	}
         OutputStream out=null;
         try {
-	        out = FileUtils.getOutputStream(targetFilePath);  
-	        client.retrieveFile(locaFilePath, out);   
+	        out = FileUtils.getOutputStream(locaFilePath);  
+	        InputStream in = client.retrieveFileStream(encode(targetFilePath));
+	        IOUtils.write(in, out, 1024);
         } finally {
         	 IOUtils.close(out);
+        	 //详见https://www.jianshu.com/p/a90cc2aeefca
+        	 client.completePendingCommand();
         }
     }  
 	
-	/**
-	 * 读取文件
-	 * @param targetPath
-	 * @param charset 默认utf-8
-	 * @return
-	 * @throws IOException
-	 */
-	public InputStream readFile(String targetPath, Charset charset) throws IOException {
-		if(charset == null) {
+    /**
+     * 读取文件
+     * @param targetPath
+     * @param charset
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public List<String> readLines(String targetPath, Charset charset) throws FileNotFoundException,IOException {
+    	if(!isFile(targetPath)) {
+    		throw new FileNotFoundException("file not existed:"+targetPath);
+    	}
+    	if(charset == null) {
 			charset = Charsets.UTF_8;
 		}
 		try {
-			client.setControlEncoding(charset.toString()); // 中文支持  
-			client.setFileType(FTPClient.BINARY_FILE_TYPE);
-//			client.changeWorkingDirectory(ftpPath);
-	        return client.retrieveFileStream(encode(targetPath));	//需要注意编码转换
-		} catch(Exception ex) {
-			throw ex;
-		} finally {
-//			IOUtils.close(in);	//IOUtils.readLines方法中已关闭该输入流
+	       InputStream in = readFile(targetPath);	//需要注意编码转换
+	       return IOUtils.readLines(in, charset);
+		}   finally {
+			//详见https://www.jianshu.com/p/a90cc2aeefca
+			client.completePendingCommand();
 		}
-	}
-	
-	public boolean isDirectory(String targetPath) throws IOException {
-		FTPFile[] files = client.listFiles(targetPath);
-		return files.length>1;
-	}
+    	
+    }
+    
+    /**
+     * 读取文件
+     * @param targetPath
+     * @param lineSparator
+     * @param charset
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public String readText(String targetPath, String lineSparator, Charset charset) throws FileNotFoundException, IOException {
+    	if(!isFile(targetPath)) {
+    		throw new FileNotFoundException("file not existed:"+targetPath);
+    	}
+    	if(charset == null) {
+			charset = Charsets.UTF_8;
+		}
+    	try {
+	       InputStream in = readFile(targetPath);	//需要注意编码转换
+	       return IOUtils.readText(in, lineSparator, charset);
+		}   finally {
+			//详见https://www.jianshu.com/p/a90cc2aeefca
+			client.completePendingCommand();
+		}
+    }
+    
+    @Deprecated
+    public InputStream readFile(String targetPath) throws IOException {
+    	return client.retrieveFileStream(encode(targetPath));	//需要注意编码转换
+    }
 	
 	/**
 	 * 获取文件()
@@ -253,7 +310,25 @@ public class FtpHelper implements Disposable {
 	 * @throws IOException 
 	 */
 	public FTPFile[] listFiles(String targetPath) throws IOException {
-		return client.listFiles(encode(targetPath));
+		FTPFile[] list = client.listFiles(encode(targetPath));
+//		if(list != null) {
+//			for (FTPFile ftpFile : list) {
+//				ftpFile.setName(decode(ftpFile.getName()));
+//			}
+//		}
+		return list;
+	}
+	
+	public boolean fileExist(String targetPath) throws IOException {
+		return client.listFiles(encode(targetPath)).length >0;
+	}
+	
+	public boolean isFile(String targetPath) throws IOException {
+		return client.listFiles(encode(targetPath)).length == 1;
+	}
+	
+	public boolean isDirectory(String targetPath) throws IOException {
+		return client.listFiles(encode(targetPath)).length > 1;
 	}
 	
 	/**
@@ -265,13 +340,12 @@ public class FtpHelper implements Disposable {
 	public synchronized boolean delete(String targetPath) {
 		
 		try {	//文件夹
-			targetPath = encode(targetPath);
-			FTPFile[] files = client.listFiles(targetPath);
+			FTPFile[] files = client.listFiles(encode(targetPath));
 			if(files.length == 0) {	//文件不存在
 				return true;
 			}
 			if(files.length == 1) {	//文件
-				return client.deleteFile(targetPath);
+				return client.deleteFile(encode(targetPath));
 			} else {	//文件夹
 				return deleteDir(targetPath, "");
 			}
@@ -282,50 +356,16 @@ public class FtpHelper implements Disposable {
 		return false;
 	}
 	
-	/*=============内部方法=================*/
-	private boolean uploadFile(String localFilePath, String targetDir) throws IOException {
-		Assert.notNull(client, "该ftpClient已经被释放");
-		Assert.notExisted(localFilePath, "需要ftp传输的文件不存在:"+localFilePath);
-		
-		try {
-			File file = new File(localFilePath);
-			if(targetDir != null) {
-				String dir = encode(targetDir);
-				client.makeDirectory(dir);
-				client.changeWorkingDirectory(dir);
-			}
-			
-			if(file.isDirectory()) {		//文件夹
-				String dir = file.getName()+File.separator;
-				File[] files = file.listFiles();
-				if(ListUtils.isEmpty(files)) {	//没有子文件
-					client.makeDirectory(encode(dir));
-				} else {
-					for (File subFile : files) {
-						boolean flag = uploadFile(subFile.getAbsolutePath(), file.getName()+File.separator);
-						if(!flag) {
-							return false;
-						}
-					}
-				}
-				
-			} else {			//文件
-				boolean flag = uploadSingleFile(localFilePath, file.getName());
-				if(!flag) {
-					Console.err("ftp传输失败:"+localFilePath);
-					return false;
-				}
-			}
-			return true;
-		} catch(Exception ex) {
-			throw ex;
-		} finally {
-			if(targetDir != null) {	//为了在操作完成后能回到根目录
-				client.changeToParentDirectory();
-			}
-		}
+	/**
+	 * 逐级创建目录
+	 * @param dirPath
+	 * @throws IOException
+	 */
+	public synchronized void mkDirs(String dirPath) throws IOException {
+		mkDirs(dirPath, true);
 	}
 	
+	/*=============内部方法=================*/
 	/**
 	 * 上传单个文件
 	 * 
@@ -372,25 +412,21 @@ public class FtpHelper implements Disposable {
 	private boolean deleteDir(String targetDir,
             String currentDir) throws IOException {
         String dirToList = targetDir;
-        if (!currentDir.equals("")) {
+        if (!currentDir.equals("") && (!currentDir.startsWith("/") && !currentDir.startsWith("\\"))) {
             dirToList += "/" + currentDir;
         }
- 
-        FTPFile[] subFiles = client.listFiles(dirToList);
- 
-        if (subFiles != null && subFiles.length > 0) {
+        
+        FTPFile[] subFiles = client.listFiles(encode(dirToList));
+		
+        if (subFiles != null && subFiles.length > 0) {;
             for (FTPFile aFile : subFiles) {
                 String currentFileName = aFile.getName();
+               
                 if (currentFileName.equals(".") || currentFileName.equals("..")) {
                     // skip parent directory and the directory itself
                     continue;
                 }
-                String filePath = StringUtils.concat(
-                		targetDir, "/", currentDir, "/", currentFileName);
-                if (currentDir.equals("")) {
-                    filePath = StringUtils.concat(targetDir, "/", currentFileName);
-                }
- 
+               
                 if (aFile.isDirectory()) {
                     // remove the sub directory
                 	boolean deleted = deleteDir(dirToList, currentFileName);
@@ -398,10 +434,18 @@ public class FtpHelper implements Disposable {
                 		return false;
                 	}
                 } else {
+                	//构造文件路径
+                	 String filePath ="";
+                     if (currentDir.equals("")) {
+                         filePath = StringUtils.concat(targetDir, "/", currentFileName);
+                     } else {
+                     	filePath = StringUtils.concat(
+                          		targetDir, "/", currentDir, "/", currentFileName);
+                     }
                     // delete the file
-                    boolean deleted = client.deleteFile(filePath);
+                    boolean deleted = client.deleteFile(encode(filePath));
                     if (!deleted) {
-                    	System.out.println("删除失败:"+filePath);
+                    	System.err.println("删除失败:"+filePath);
                     	return false;
                     }
                 }
@@ -410,13 +454,65 @@ public class FtpHelper implements Disposable {
         }
         
         // finally, remove the directory itself
-        boolean removed = client.removeDirectory(dirToList);
+        boolean removed = client.removeDirectory(encode(dirToList));
         if (!removed) {
-        	System.out.println("删除目录失败:"+dirToList);
+        	System.err.println("删除目录失败:"+dirToList);
         	return false;
         }
         return true;
     }
+	
+	/**
+	 * 逐级创建目录(失败时回退到原来的工作路径)
+	 * @param dirPath 目标路径
+	 * @param rebackWorkingDir 是否在创建成功时返回原来的工作目录
+	 * @throws IOException
+	 */
+	private void mkDirs(String dirPath, boolean rebackWorkingDir) throws IOException {
+		String currentWorkingDir = client.printWorkingDirectory();
+		try {
+			dirPath = encode(dirPath);
+			if(changeWorkingDirectory(encode(dirPath))) {
+				return;
+			}
+			String[] group = getFileTree(dirPath);
+			
+			for (String dirName : group) {
+				if (!changeWorkingDirectory(dirName)) {
+					if(!client.makeDirectory(dirName)) {
+						throw new ValidateException("创建目录:"+dirName+"失败");
+					}
+					if(!changeWorkingDirectory(dirName)) {
+						throw new ValidateException("切换至目录:"+dirName+"失败");
+					}
+				}
+			}
+		} catch (ValidateException e) {	//创建失败的情况返回根目录
+			changeWorkingDirectory(currentWorkingDir);
+			throw new IOException(e.getMessage());
+		} catch(Throwable t) {
+			changeWorkingDirectory(currentWorkingDir);
+			throw t;
+		} finally {
+			if(rebackWorkingDir) {
+				changeWorkingDirectory(currentWorkingDir);
+			}
+		}
+	}
+	
+	/**
+	 * "/a/b/c".split("[/\\\\]+")
+	 * @param dirPath 目录路径
+	 * @return 目录层级
+	 */
+	private String[] getFileTree(String dirPath) {
+		dirPath = dirPath.replaceFirst("^[/\\\\]", "");
+		return dirPath.split("[/\\\\]+");
+	}
+	
+	private boolean changeWorkingDirectory(String pathname) throws IOException {
+		return client.changeWorkingDirectory(pathname);
+	}
 	
 	/**
 	 * 转换文件名/路径编码
@@ -430,6 +526,12 @@ public class FtpHelper implements Disposable {
 		return fileName;
 	}
     
+//	private String decode(String fileName) {
+//		if(encodingMode) {
+//			return new String(fileName.getBytes(ENCODINGS[1]), ENCODINGS[0]);
+//		}
+//		return fileName;
+//	}
 	
 	public static void main(String[] args) {
 		FtpHelper helper = null;
