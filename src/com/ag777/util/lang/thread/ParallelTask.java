65e3f5d1dcb71c85184c6ecbdce08ea35544f8c0
ag777 <aaaggg777@qq.com>
2023-08-15 17:18:24 +0800

    调整ParallelTask实现并增加部分注释

--- src/com/ag777/util/lang/thread/ParallelTask.java
+++ src/com/ag777/util/lang/thread/ParallelTask.java
@@ -4,10 +4,7 @@ import com.ag777.util.lang.model.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -31,6 +28,12 @@ public abstract class ParallelTask<T, D, R, V, E extends Exception> implements C
 
     private volatile R result;
 
+    /**
+     *
+     * @param executeService 线程池
+     * @param initResult 返回智初始值
+     * @param batchSize 一次性处理的数据数量
+     */
     public ParallelTask(ExecutorService executeService, R initResult, int batchSize) {
         this.pool = executeService;
         vsh = new CompletionServiceHelper<>(executeService);
@@ -47,11 +50,29 @@ public abstract class ParallelTask<T, D, R, V, E extends Exception> implements C
         return pool;
     }
 
+    /**
+     * 将该类放到和其它任务同一个线程池里执行
+     * @return 异步任务
+     */
+    public Future<R> runInPool() {
+        return pool.submit(this);
+    }
+
+    /**
+     *
+     * @param callable 任务
+     * @param bindData 要绑定的数据
+     * @return self
+     */
     public ParallelTask<T, D, R, V, E> add(Callable<T> callable, D bindData) {
         vsh.submit(callable, bindData);
         return this;
     }
 
+    /**
+     * 添加完所有任务后务必调用该方法，避免call()先入无限等待状态
+     * @return self
+     */
     public ParallelTask<T, D, R, V, E> finishAdd() {
         taskAddFinished.set(true);
         return this;
@@ -68,6 +89,11 @@ public abstract class ParallelTask<T, D, R, V, E extends Exception> implements C
         }
     }
 
+    /**
+     * 等待并处理任务结果
+     * @throws InterruptedException 中断异常
+     * @throws E 自定义异常
+     */
     private void waitAndHandleTask() throws InterruptedException, E {
         List<Pair<T,D>> list = new ArrayList<>(batchSize);
         try {
@@ -114,7 +140,7 @@ public abstract class ParallelTask<T, D, R, V, E extends Exception> implements C
         }
     }
 
-    private void handleOnce(List<Pair<T, D>> pairs) throws E {
+    private void handleOnce(List<Pair<T, D>> pairs) throws InterruptedException, E {
         // 到达批量处理数量阈值，执行处理
         V newVal = handleItems(pairs);
         synchronized (ParallelTask.class) {
@@ -122,12 +148,16 @@ public abstract class ParallelTask<T, D, R, V, E extends Exception> implements C
         }
     }
 
+    /**
+     * 当任务当前任务就已经添加完毕，触发一批任务结果的处理后调用盖方法
+     * @param taskCount 剩余任务数量
+     */
     protected void whenCountDown(int taskCount) {
     }
 
-    public abstract V handleItems(List<Pair<T,D>> items) throws E;
+    public abstract V handleItems(List<Pair<T,D>> items) throws InterruptedException, E;
 
-    public abstract void onErr(D bindData, Throwable t) throws E;
+    public abstract void onErr(D bindData, Throwable t) throws InterruptedException, E;
 
     public abstract R merge(R result, V newVal);
 
