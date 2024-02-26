17273a48217495d94b66739acb2b6bfecb3efed2
ag777 <aaaggg777@qq.com>
2024-02-26 09:02:57 +0800

    修复 RobotUtils中的captureFullScreen方法在高dpi的显示屏中只能截取屏幕左上部分的bug

--- src/com/ag777/util/lang/RobotUtils.java
+++ src/com/ag777/util/lang/RobotUtils.java
@@ -9,7 +9,7 @@ import java.awt.image.BufferedImage;
  * 用于模拟鼠标和键盘操作，以及捕获屏幕截图。
  *
  * @author ag777 <837915770@vip.qq.com>
- * @version 2024/1/17 14:34
+ * @version 2024/2/26 09:00
  */
 public class RobotUtils {
 
@@ -103,9 +103,11 @@ public class RobotUtils {
      * @return 返回一个 BufferedImage，包含了整个屏幕的截图
      */
     public static BufferedImage captureFullScreen(Robot robot) {
-        // 获取屏幕的尺寸
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        // 获取屏幕的尺寸,修复高dpi下只截取了屏幕左上不分的问题
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();
         // 使用屏幕尺寸作为参数，调用 captureScreen 方法捕获全屏截图
-        return captureScreen(robot, 0, 0, screenSize.width, screenSize.height);
+        return captureScreen(robot, 0, 0, width, height);
     }
 }
